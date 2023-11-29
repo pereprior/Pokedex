a822b3b026a41912785f920ca8a95218d3d6c80e
pprior <alu5332@ieselcaminas.org>
2023-11-29 15:17:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pokedex2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.pokedex2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.pokedex2.ui.theme.Pokedex2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Pokedex2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
