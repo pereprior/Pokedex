0b8c0061bf939d740dfe805efec67486ffdb2e25
rauls <alusim0931@ieselcaminas.org>
2023-12-21 19:30:24 +0100

    Update

---
+++ app/src/main/java/com/example/pokedex2/ui/view/list/MapsData.kt
@@ -0,0 +1,51 @@
+package com.example.pokedex2.ui.view.list
+
+import com.example.pokedex2.R
+
+data class TestDataItem(
+    var id: String,
+    var title: String,
+    var drawable: Int
+)
+
+
+object mapsData {
+    val imageList = listOf(
+        R.drawable.mapaalola,
+        R.drawable.mapagalar,
+        R.drawable.mapahoenn,
+        R.drawable.mapajhoto,
+        R.drawable.mapakalos,
+        R.drawable.mapakanto,
+        R.drawable.mapapaldea,
+        R.drawable.mapasinnoh,
+        R.drawable.mapateselia
+    )
+
+    val mapNames = listOf(
+        "Alola",
+        "Galar",
+        "Hoenn",
+        "Jhoto",
+        "Kalos",
+        "Kanto",
+        "Paldea",
+        "Sinnoh",
+        "Teselia"
+    )
+
+    val testmapsData: List<TestDataItem> by lazy {
+        generateTestData()
+    }
+
+    private fun generateTestData(): List<TestDataItem> {
+        return imageList.mapIndexed { index, drawable ->
+            TestDataItem(
+                id = (index + 1).toString(),
+                title = mapNames[index],
+                drawable = drawable
+            )
+        }
+    }
+}
+
